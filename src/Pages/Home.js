3d5f47f5a28180e41090c06fb35fde15c58acec6
diamond-nicholas <diamondnicholas154@gmail.com>
2022-07-03 13:44:37 +0100

    add home banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
+import './Home.scss';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <main>
+        <section className='banner'>
+          <h1>RenGlobal Properties</h1>
+          <h3>
+            The largest real estate company for home owners, buyers and partners
+          </h3>
+          <h4>Making the incredible affordable</h4>
+          <p>
+            Begin your journey and become a property owner today{' '}
+            <span>contact us</span>
+          </p>
+        </section>
+      </main>
     </div>
   );
 };
