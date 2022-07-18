12e4f3569c0c8a231831c0dae05ba290fc6178d6
diamond-nicholas <diamondnicholas154@gmail.com>
2022-07-18 15:12:30 +0100

    add why choose us section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -54,6 +54,18 @@ const About = () => {
             </article>
           </section>
         </section>
+        <section className='why_choose_us'>
+          <h3>Why Choose Us</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            fugit et expedita eius labore? Praesentium laudantium laborum
+            perferendis veniam provident neque vel nobis, assumenda, eveniet
+            nesciunt dignissimos modi illo necessitatibus libero soluta
+            cupiditate voluptatem minima dolore eligendi adipisci.
+            Exercitationem sunt iste odio eos illo. Obcaecati impedit possimus
+            excepturi eveniet laudantium?
+          </p>
+        </section>
       </div>
     </div>
   );
